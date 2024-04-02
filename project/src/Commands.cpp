5e8276c77af565de8dd1c2bc4392461f162684b2
tehuanmelo <tehuanmelo@gmail.com>
2024-04-02 12:08:47 +0400

    bla

---
+++ project/src/Commands.cpp
@@ -0,0 +1,5 @@
+#include "../inc/t_Server.hpp"
+
+std::string Server::joinChannel(Request req, int fd) {
+
+}
---
+++ project/src/t_main.cpp
@@ -0,0 +1,15 @@
+#include "../inc/common.hpp"
+#include "../inc/Channel.hpp"
+#include "../inc/t_Server.hpp"
+#include "../inc/Parser.hpp"
+
+class User;
+class Channel;
+class Server;
+class Parser;
+
+
+int main() {
+    Server server;
+
+}
