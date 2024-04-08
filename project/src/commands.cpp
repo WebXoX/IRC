20b94e70e323988d38dfe2e401235e974230e90f
Joe Shaju Perinchery <jperinch@lab2r3s3.42abudhabi.ae>
2024-04-08 17:21:23 +0400

    testing merge

--- project/src/commands.cpp
+++ project/src/commands.cpp
@@ -119,16 +119,16 @@ int Server::register_user(ircMessage msg, Client * user)
         else
         {
             user->nickname = msg.params[0];
-            // this->nicknames.push_back(msg.params[0]);
-            // user->regi_status = 5;
-            // if(user->nickname.empty() == false)
-            // {
-            //     this->nicknames.erase(std::find(this->nicknames.begin(), this->nicknames.end(), user->nickname));
-            //     str = this->msg("irssi", NULL,user->nickname,"NICK "+ msg.params[0]).c_str();
-            //     len = str.length();
-            //     send(user->client_fd,str.c_str(),len,0);
-            //     user->nickname.clear();
-            // }
+            this->nicknames.push_back(msg.params[0]);
+            user->regi_status = 5;
+            if(user->nickname.empty() == false)
+            {
+                this->nicknames.erase(std::find(this->nicknames.begin(), this->nicknames.end(), user->nickname));
+                str = this->msg("irssi", NULL,user->nickname,"NICK "+ msg.params[0]).c_str();
+                len = str.length();
+                send(user->client_fd,str.c_str(),len,0);
+                user->nickname.clear();
+            }
         }
         return 1;
     }
